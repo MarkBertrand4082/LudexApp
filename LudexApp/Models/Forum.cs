0f19828339a22c35a47c04d4c0fb82c5cd51c308
Lew Lin <lew100424@hotmail.com>
2025-11-24 13:06:15 -0500

    Made basic stuff for the Forum, Post and User Model

--- LudexApp/Models/Forum.cs
+++ LudexApp/Models/Forum.cs
@@ -2,5 +2,41 @@
 {
     public class Forum
     {
+        public /*Insert game class*/void game { get; set; }
+
+        public string name { get; set; }
+
+        private List<Post> posts;
+
+        public int id { get; set; }
+
+        public Post getPost(int _id)
+        {
+            foreach(Post post in posts)
+            {
+                if (post.id == _id)
+                {
+                    return post;
+                }
+            }
+            return null;
+        }
+
+        public void AddPost(Post post)
+        {
+            posts.Add(post);
+        }
+
+        public void RemovePost(int _id)
+        {
+            foreach (Post post in posts)
+            {
+                if (post.id == _id)
+                {
+                    posts.Remove(post);
+                    break;
+                }
+            }
+        }
     }
 }
--- LudexApp/Models/Post.cs
+++ LudexApp/Models/Post.cs
@@ -2,5 +2,19 @@
 {
     public class Post
     {
+        public int id { get; set; }
+
+        public string title { get; set; }
+
+        public string content { get; set; }
+
+        public Forum forum { get; set; }
+
+        public Post(string _title, string _content, Forum _forum)
+        {
+            this.title = _title;
+            this.content = _content;
+            this.forum = _forum;
+        }
     }
 }
